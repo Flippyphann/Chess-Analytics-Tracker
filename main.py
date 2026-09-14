355071f9a8288d1a49ffffee8c1e9a1ab941adf0
Phillip Phan <phillipphan11@gmail.com>
2026-09-14 00:43:33 -0400

    Added Chess.com ALL games retrieval

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ USERNAME = "phillipphan11"
 
 player = get_player(USERNAME)
 archives = get_archives(USERNAME)
-games = get_games_from_archive(archives[0])
+
+all_games = []
+
+for archive in archives:
+    games = get_games_from_archive(archive)
+    all_games.extend(games)
+
+print(f"Total games: {len(all_games)}")
+
 
 print(player)
 print(archives)
-print(games)
