e574109415bd849d7bc67d2adb78f34a9ce04230
Phillip Phan <phillipphan11@gmail.com>
2026-09-13 23:30:46 -0400

    Added Chess.com game archive retrieval

--- api/chesscom.py
+++ api/chesscom.py
@@ -8,8 +8,16 @@ HEADERS = {
 
 def get_player(username: str) -> dict:
     url = f"{BASE_URL}/player/{username}"
-
     response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
 
     return response.json()
+
+def get_archives(username: str) -> list:
+    url = f"{BASE_URL}/player/{username}/games/archives"
+    response = requests.get(url, headers=HEADERS)
+    response.raise_for_status()
+
+    data = response.json()
+    return data["archives"]
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from api.chesscom import get_player
+from api.chesscom import get_player, get_archives
 
 USERNAME = "phillipphan11"
 
 player = get_player(USERNAME)
+games = get_archives(USERNAME)
 
 print(player)
+print(games)
