d04bb882bc22d02057fe670e9b931ca57681ad44
Phillip Phan <phillipphan11@gmail.com>
2026-09-15 11:11:55 -0400

    Added raw Chess.com game data storage

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from api.chesscom import get_player, get_archives, get_games_from_archive
 
 USERNAME = "phillipphan11"
@@ -12,6 +14,9 @@ for archive in archives:
     all_games.extend(games)
 
 print(f"Total games: {len(all_games)}")
+
+with open(f"data/raw/{USERNAME}.json", "w", encoding="utf-8") as file:
+    json.dump(all_games, file, indent=2)
 
 
 print(player)
