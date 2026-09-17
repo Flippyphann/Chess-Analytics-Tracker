415b78bbc68faaabb98bc9608b0bf7e413720cc6
Phillip Phan <phillipphan11@gmail.com>
2026-09-16 22:18:25 -0400

    Added game & move data models

---
+++ models/game.py
@@ -0,0 +1,22 @@
+class Game:
+    def __init__(
+        self,
+        white: str,
+        black: str,
+        result: str,
+        date: str,
+        time_control: str,
+        opening: str,
+        termination: str,
+        moves: list[str]
+    ):
+        self.white = white
+        self.black = black
+        self.result = result
+        self.date = date
+        self.time_control = time_control
+        self.opening = opening
+        self.termination = termination
+        self.moves = moves
+
+
---
+++ models/move.py
@@ -0,0 +1,8 @@
+class Move:
+    def __init__(
+        self,
+        move_number: int,
+        san: str
+    ):
+        self.move_number = move_number
+        self.san = san
