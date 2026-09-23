aae4e3497baf8e9673413aaccea6d7f86cfc36f9
Phillip Phan <phillipphan11@gmail.com>
2026-09-23 11:15:14 -0400

    Added move improvement detection

--- analysis/stockfish.py
+++ analysis/stockfish.py
@@ -1,7 +1,7 @@
 import chess
 import chess.engine
 from models.game import Game
-from metrics.move_features import is_best_move, classify_move, is_critical, is_top_3_move, is_top_5_move
+from metrics.move_features import is_best_move, classify_move, is_critical, is_top_3_move, is_top_5_move, is_improvement
 
 
 STOCKFISH_PATH = "/opt/homebrew/bin/stockfish"
@@ -102,6 +102,9 @@ def analyze_game(engine, game: Game):
         # Checks if the move caused a significant evaluation change.
         is_critical_move = is_critical(evaluation_change)
 
+        # Checks if the player's position improved after the move.
+        is_improvement_move = is_improvement(evaluation_change)
+
         results.append({
             "move_number": move.move_number,
             "move": move.san,
@@ -115,6 +118,7 @@ def analyze_game(engine, game: Game):
             "evaluation_change": evaluation_change,
             "evaluation_loss": evaluation_loss,
             "is_critical": is_critical_move,
+            "is_improvement": is_improvement_move
         })
 
     return results
--- metrics/move_features.py
+++ metrics/move_features.py
@@ -33,3 +33,7 @@ def is_top_5_move(board: chess.Board, move: str, top_moves: list[chess.Move]) ->
     played_move = board.parse_san(move)
 
     return played_move in top_moves
+
+# Checks if the player's position improved after the move.
+def is_improvement(evaluation_change: float) -> bool:
+    return evaluation_change > 0
